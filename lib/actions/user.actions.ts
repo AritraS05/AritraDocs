b197fe7f5c55ecaad8bed429c1c83cdf7592c4f3
Aritra Sarkar <aritrasarkar00007@gmail.com>
2024-08-04 15:07:16 +0530

    tagging created

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,9 @@
 'use server';
 
-import { clerkClient } from "@clerk/nextjs/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
+import { RoomContext } from "@liveblocks/react/suspense";
+import { liveblocks } from "../liveblocks";
 
 export const getClerkUser = async ({userIds} : {userIds: string[]}) => {
   try {
@@ -23,3 +25,17 @@ export const getClerkUser = async ({userIds} : {userIds: string[]}) => {
     console.log(`error fetching user: ${error}`);
   }
 };
+
+export const getDocumentUsers = async ({roomId, currentUser, text}:{roomId: string, currentUser: string, text: string}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+
+    if(text.length){
+      const lowerCaseText = text.toLowerCase();
+      const filteredUsers = users.filter((email : string) => email.toLowerCase().includes(lowerCaseText));
+    }
+  } catch (error) {
+    console.log(`error fetching user: ${error}`);
+  }
+}
